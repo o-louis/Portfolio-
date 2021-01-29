0aa4c134e34bd5d32b3ab08e152470f917eeb8d8
Oriane <oriane.louis.pro@gmail.com>
2021-01-29 00:30:43 -0500

    feat: Make the home

---
+++ components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="w-full flex justify-between items-center">
+    <span class="w-3/12 mr-3" />
+    <ul class="flex flex-1 justify-between">
+      <li
+        v-for="(section, index) in menu"
+        :key="index"
+        class="text-white uppercase font-normal"
+      >
+        {{ section.title }}
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      menu: [{ title: 'Home' }, { title: 'About' }, { title: 'Projects' }],
+    }
+  },
+}
+</script>
+
+<style scoped>
+span {
+  height: 1px;
+  background: rgba(255, 255, 255, 0.15);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,66 @@
-<template></template>
+<template>
+  <div class="w-10/12 m-auto py-6 relative h-screen">
+    <Header />
+    <section class="text-white mt-12">
+      <p class="opacity-60">Hello, I'm</p>
+      <h1 class="text-3xl ml-4 font-bold">ORIANE LOUIS,</h1>
+      <h2 class="text-xl ml-12 font-medium opacity-60">Front-End Developer.</h2>
+
+      <p class="mt-12 font-light opacity-60">
+        I’m a <span class="font-medium">passionate developer</span> based in
+        Montreal. I’m enthusiast about new technologies and I
+        <span class="font-medium">love building things</span> and
+        <span class="font-medium">improve user experience</span>.
+      </p>
+
+      <div class="flex justify-center mt-12">
+        <a v-for="(social, index) in socialNetwork" :key="index"
+          ><img
+            :src="social.icon"
+            :class="[socialNetwork.length - 1 != index ? 'pr-3' : '', 'h-9']"
+        /></a>
+      </div>
+    </section>
+    <div class="absolute bottom-3 left-2/4">
+      <img :src="chevronDown" class="m-auto" />
+    </div>
+  </div>
+</template>
 
 <script>
+import Header from '../components/Header.vue'
+
 export default {
   name: 'Home',
+  components: {
+    Header,
+  },
   data() {
-    return {}
+    return {
+      socialNetwork: [
+        { icon: require('../assets/linkedin.svg') },
+        { icon: require('../assets/github.svg') },
+        { icon: require('../assets/codepen.svg') },
+        { icon: require('../assets/mail.svg') },
+      ],
+      chevronDown: require('../assets/chevron-down.svg'),
+    }
   },
 }
 </script>
 
-<style></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;700&display=swap');
+
+:root {
+  --background: #101014;
+}
+
+* {
+  font-family: 'Heebo', sans-serif;
+}
+
+body {
+  background: var(--background);
+}
+</style>
