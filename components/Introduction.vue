990c42e9c16f52c3aee5809fb258c19bd13dad50
Oriane <oriane.louis.pro@gmail.com>
2021-01-29 20:05:03 -0500

    refactor: Create a component for the introduction

---
+++ components/Introduction.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="text-white mt-12">
+    <p class="opacity-60 text-lg">Hello, I'm</p>
+    <h1 class="text-4xl ml-4 font-bold">ORIANE LOUIS,</h1>
+    <h2 class="text-2xl ml-12 font-medium opacity-60">Front-End Developer.</h2>
+
+    <p class="mt-12 font-light opacity-60 text-lg">
+      I’m a <span class="font-medium">passionate developer</span> based in
+      Montreal. I’m enthusiast about new technologies and I
+      <span class="font-medium">love building things</span> and
+      <span class="font-medium">improve user experience</span>.
+    </p>
+
+    <div class="flex justify-center mt-12">
+      <a v-for="(social, index) in socialNetwork" :key="index"
+        ><img
+          :src="social.icon"
+          :class="[socialNetwork.length - 1 != index ? 'pr-3' : '', 'h-10']"
+      /></a>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Introduction',
+  data() {
+    return {
+      socialNetwork: [
+        { icon: require('../assets/linkedin.svg') },
+        { icon: require('../assets/github.svg') },
+        { icon: require('../assets/codepen.svg') },
+        { icon: require('../assets/mail.svg') },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,8 @@
 <template>
   <div class="w-10/12 m-auto py-6 relative h-screen">
     <Header />
-    <section class="text-white mt-12">
-      <p class="opacity-60 text-lg">Hello, I'm</p>
-      <h1 class="text-4xl ml-4 font-bold">ORIANE LOUIS,</h1>
-      <h2 class="text-2xl ml-12 font-medium opacity-60">
-        Front-End Developer.
-      </h2>
-
-      <p class="mt-12 font-light opacity-60 text-lg">
-        I’m a <span class="font-medium">passionate developer</span> based in
-        Montreal. I’m enthusiast about new technologies and I
-        <span class="font-medium">love building things</span> and
-        <span class="font-medium">improve user experience</span>.
-      </p>
-
-      <div class="flex justify-center mt-12">
-        <a v-for="(social, index) in socialNetwork" :key="index"
-          ><img
-            :src="social.icon"
-            :class="[socialNetwork.length - 1 != index ? 'pr-3' : '', 'h-10']"
-        /></a>
-      </div>
-    </section>
-    <div class="absolute bottom-3 w-full">
+    <Introduction />
+    <div class="chevron absolute bottom-10 w-full">
       <img :src="chevronDown" class="m-auto" />
     </div>
   </div>
@@ -31,20 +10,16 @@
 
 <script>
 import Header from '../components/Header.vue'
+import Introduction from '@/components/Introduction.vue'
 
 export default {
   name: 'Home',
   components: {
     Header,
+    Introduction,
   },
   data() {
     return {
-      socialNetwork: [
-        { icon: require('../assets/linkedin.svg') },
-        { icon: require('../assets/github.svg') },
-        { icon: require('../assets/codepen.svg') },
-        { icon: require('../assets/mail.svg') },
-      ],
       chevronDown: require('../assets/chevron-down.svg'),
     }
   },
@@ -64,5 +39,21 @@ export default {
 
 body {
   background: var(--background);
+}
+
+.chevron {
+  animation-duration: 0.8s;
+  animation-name: bounce;
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+}
+
+@keyframes bounce {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
 }
 </style>
