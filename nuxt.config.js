edb671715e2c5919b025d43739ea1f0a0462133f
o-louis <oriane.louis.pro@gmail.com>
2022-09-11 23:08:50 -0400

    chore: Change page description

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
 				name: 'viewport',
 				content: 'width=device-width, initial-scale=1',
 			},
-			{ hid: 'description', name: 'description', content: '' },
+			{ hid: 'description', name: 'description', content: 'Front-End Developer based in Montreal' },
 		],
 		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
 	},
